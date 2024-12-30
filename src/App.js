6f02396f8049b66f8c4ef3610017b11c52c30dcb
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-30 12:34:49 -0300

    feat: imports components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject';
+
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/company'>
+          <Company />
+        </Route>
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
+        <Route exact path='/newproject'>
+          <NewProject />
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
