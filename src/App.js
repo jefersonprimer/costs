48a976d089138d8d48062cf8a1960ff95e0931ad
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-30 12:56:12 -0300

    feat: layout and container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject';
 
+import Container from './components/layout/Container';
+
 import './App.css'
 
 function App() {
@@ -16,18 +18,20 @@ function App() {
         <Link to="/newproject">New Project</Link>
       </div>
       <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/company'>
-          <Company />
-        </Route>
-        <Route exact path='/contact'>
-          <Contact />
-        </Route>
-        <Route exact path='/newproject'>
-          <NewProject />
-        </Route>
+        <Container customClass="min-height">
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/company'>
+            <Company />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+          <Route exact path='/newproject'>
+            <NewProject />
+          </Route>
+        </Container>
       </Switch>
       <p>Footer</p>
     </Router>
